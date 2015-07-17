4496a4a34d4fd4bab0d985fab6302182e50d16d4
Sebastian Thiel <byronimo@gmail.com>
2015-07-17 11:06:05 +0200

    Added note about item04

---
+++ src/main/rust/chapter02/item04/src/lib.rs
@@ -0,0 +1,8 @@
+
+
+#[test]
+/// This item is just a Java-utility class that prevents any kind of 
+/// object instantiation through a throwing private constructor.
+/// This doesn't apply to Rust at all.
+fn it_works() {
+}
